a5791c4fba3d46ffffd8b75ebadc1ac29a54cece
Idi Codes <eemailmii@gmail.com>
2024-03-19 16:52:18 +0800

    feat: added drizzle as orm

---
+++ lib/schema.ts
@@ -0,0 +1,19 @@
+import { boolean, pgTable, timestamp, varchar } from 'drizzle-orm/pg-core';
+
+export const users = pgTable('users', {
+  id: varchar('id').primaryKey(),
+  email: varchar('email').notNull(),
+  password: varchar('password').notNull(),
+  emailVerified: boolean('email_verified').default(false),
+  createdAt: timestamp('created_at', {
+    mode: 'date',
+    withTimezone: true,
+  }).defaultNow(),
+  updatedAt: timestamp('updated_at', {
+    mode: 'date',
+    withTimezone: true,
+  }),
+});
+
+export type User = typeof users.$inferSelect;
+export type NewUser = typeof users.$inferInsert;

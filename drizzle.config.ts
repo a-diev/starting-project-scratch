bd5aa051b7bf4a2e64ecfc4da7a145cb53327382
Idicodes <eemailmii@gmail.com>
2024-04-05 16:51:34 +0800

    refactor: structuring the source code

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'drizzle-kit';
 
 export default {
   schema: './lib/schema.ts',
-  out: './drizzle',
+  out: './lib/database/migration',
   driver: 'pg',
   dbCredentials: {
     connectionString: process.env.DATABASE_URL!,
